e8e06d8bc48111943eeee83ad378c5c33d0b67b0
Ricardo García <rich4rd3rd@gmail.com>
2026-01-29 00:42:18 -0500

    Create Player in progress

---
+++ src/main/java/com/tabletennis/service/PlayerService.java
@@ -0,0 +1,10 @@
+package com.tabletennis.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class PlayerService {
+}
