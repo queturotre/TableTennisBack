81a3762292b8a4cce1b375f2c9e6344666f6012e
Ricardo García <rich4rd3rd@gmail.com>
2026-01-30 02:13:56 -0500

    Create Player service working

--- src/main/java/com/tabletennis/controller/PlayerController.java
+++ src/main/java/com/tabletennis/controller/PlayerController.java
@@ -1,12 +1,13 @@
 package com.tabletennis.controller;
 
+import com.tabletennis.DTO.CreatePlayerDTO;
 import com.tabletennis.DTO.PlayerDTO;
+import com.tabletennis.entity.Player;
 import com.tabletennis.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,4 +27,13 @@ public class PlayerController {
         return ResponseEntity.ok(players);
     }
 
+    @PostMapping("/create-player")
+    public ResponseEntity<Player> createPlayer(@RequestBody CreatePlayerDTO dto){
+        try {
+            Player player = playerService.createPlayer(dto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(player);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 }
