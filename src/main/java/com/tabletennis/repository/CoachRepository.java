53eed4da92a91a1103e899f15b11e8cadc2094b7
Ricardo García <rich4rd3rd@gmail.com>
2026-01-31 01:28:11 -0500

    Ready to find coaches by club id

--- src/main/java/com/tabletennis/repository/CoachRepository.java
+++ src/main/java/com/tabletennis/repository/CoachRepository.java
@@ -4,13 +4,11 @@ import com.tabletennis.DTO.CoachDTO;
 import com.tabletennis.entity.Coach;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CoachRepository extends JpaRepository<Coach, Integer> {
-    @Query("""
-           SELECT new com.tabletennis.DTO.CoachDTO(c.idCoach, c.club.idClub, c.name)
-           FROM Coach c
-           """)
+    @Query("SELECT new com.tabletennis.DTO.CoachDTO(c.idCoach, c.club.idClub, c.name) FROM Coach c")
     List<CoachDTO> findAllCoaches();
 }
