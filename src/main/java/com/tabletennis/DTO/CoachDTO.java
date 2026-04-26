c273e068b39d952f8fc353e171021e8fbe1721ef
Ricardo García <rich4rd3rd@gmail.com>
2026-04-26 02:29:37 -0500

    CoachDTO built with Integer instead of int

--- src/main/java/com/tabletennis/DTO/CoachDTO.java
+++ src/main/java/com/tabletennis/DTO/CoachDTO.java
@@ -1,23 +1,23 @@
 package com.tabletennis.DTO;
 
 public class CoachDTO {
-    private int id;
+    private Integer id;
     private Integer idClub;
     private String name;
 
     public CoachDTO() {}
 
-    public CoachDTO(int id, int idClub, String name) {
+    public CoachDTO(Integer id, Integer idClub, String name) {
         this.id = id;
         this.idClub = idClub;
         this.name = name;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
