4833d472c6c5ee0281c23bf5fcde7ccf7c21a910
queturotre <rich4rd3rd@gmail.com>
2026-05-19 23:58:57 -0500

    Repository cleaning started

--- src/main/java/com/tabletennis/repository/UserRoleRepository.java
+++ src/main/java/com/tabletennis/repository/UserRoleRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
     Optional<UserRole> findByRoleName(String roleName);
 }
